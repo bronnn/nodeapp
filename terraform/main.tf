1a144510eec84b2735754bb802024018a4749cb6
Bron M <bronmmatthews@gmail.com>
2024-04-23 21:09:55 +0100

    test fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,7 +50,7 @@ resource "aws_security_group" "maingroup" {
   ]
   ingress = [
     {
-      cidr_blocks      = ["0.0.0.0/0", ]
+      cidr_blocks      = ["0.0.0.0/0"]
       description      = ""
       from_port        = 22
       ipv6_cidr_blocks = []
@@ -61,7 +61,7 @@ resource "aws_security_group" "maingroup" {
       to_port          = 22
     },
     {
-      cidr_blocks      = ["0.0.0.0/0", ]
+      cidr_blocks      = ["0.0.0.0/0"]
       description      = ""
       from_port        = 80
       ipv6_cidr_blocks = []
